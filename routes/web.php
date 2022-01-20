e7f61394bcca548ecd358229be3e560b931cecbf
Fenext04 <mateusmelo04@gmail.com>
2022-01-20 13:28:51 -0300

    Mano, nem sei

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/',function(){
     return view("auth.login");
 });
 
-Route::get('/cadastrar', function () {
+Route::get('cadastrar', function () {
     return view('auth.register');
 })->name("visitante.cadastro");
 
